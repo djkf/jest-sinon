9fc539a946cbe415af3aedec0a0f79c1e0c1c38a
djkf <fox.darrel@googlemail.com>
2019-09-06 20:27:40 +0100

    build: commitizen

--- packages/jest-sinon/rollup.config.js
+++ packages/jest-sinon/rollup.config.js
@@ -4,7 +4,6 @@ import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
 
 export default [
-  // browser-friendly UMD build
   {
     input: "src/index.js",
     output: {
@@ -12,27 +11,12 @@ export default [
       file: pkg.browser,
       format: "umd"
     },
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      babel()
-    ]
+    plugins: [resolve(), commonjs(), babel()]
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: "src/index.js",
     external: ["sinon"],
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      babel()
-    ],
+    plugins: [resolve(), commonjs(), babel()],
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" }
