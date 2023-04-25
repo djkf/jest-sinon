97ad26bbd175ecfd444fbd2b4d4412467e72d157
Segev Finer <segev@swimm.io>
2023-04-25 19:30:28 +0300

    Enable usage with @playwright/test
    I only tested using it with @playwright/test, not sure if there are further adjustments needed for Vitest as it has it's own spies and matchers for them

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,26 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
 
 export default [
   {
-    input: 'src/index.js',
-    external: [
-      'jest-matcher-utils'
-    ],
+    input: { 'jest-sinon': 'src/index.js', 'assertions': 'src/assertions/index.js' },
+    external: ['jest-matcher-utils'],
     plugins: [
       resolve(),
       commonjs(),
       babel({
         exclude: ['node_modules/**']
-      }),
+      })
     ],
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { dir: 'dist/cjs', format: 'cjs' },
+      {
+        dir: 'dist/esm',
+        format: 'es',
+        entryFileNames: '[name].mjs',
+        chunkFileNames: '[name]-[hash].mjs'
+      }
     ]
   }
 ];
